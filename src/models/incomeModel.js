dd52add3315763182e1b5525abc3fb4f3b612192
DelinVijay <152050629+DelinVijay@users.noreply.github.com>
2024-08-20 10:30:15 +0530

    Create incomeModel.js

---
+++ src/models/incomeModel.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+const Schema=require('mongoose').Schema;
+
+const incomeSchema=new Schema(
+   [ {
+    "income":Number,
+    "date":Date,
+     }
+]
+);
+
+const incomeModel=mongoose.model("incomes",incomeSchema);
+
+module.exports=incomeModel;
