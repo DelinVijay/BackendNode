e9cee3b8f9fca8ca0ed29f8298149593e76fb68b
DelinVijay <152050629+DelinVijay@users.noreply.github.com>
2024-08-21 09:50:40 +0530

    Update expenseExpressController.js

--- src/controller/expenseExpressController.js
+++ src/controller/expenseExpressController.js
@@ -1,7 +1,6 @@
 
-const expenseModel=require('../models/expenseModel')
-const incomeModel=require('../models/incomeModel')
-
+const expenseModel=require('../models/expenseModel');
+const incomeModel =require('../models/incomeModel');
 const readExpense=async (req,res)=>{
     try{
     let expense = await expenseModel.find();
@@ -13,19 +12,19 @@ const readExpense=async (req,res)=>{
         console.log({"data":"","msg":"","err":err.message})
         res.status(500).json({"data":"","message":"","err":err.message}); 
     }
-}
+} 
 const readIncome=async (req,res)=>{
     try{
     let income = await incomeModel.find();
-    (income.length>0)?
-        res.status(200).json({"data":income,"message":"","err":""})
-    :
-        res.status(400).json({"data":"","message":"no user with that id","err":""});
-    }catch(err){
-        console.log({"data":"","msg":"","err":err.message})
-        res.status(500).json({"data":"","message":"","err":err.message}); 
-    }
-}
+     (income.length>0)?
+         res.status(200).json({"data":income,"message":"","err":""})
+     :
+         res.status(400).json({"data":"","message":"no user with that id","err":""});
+     }catch(err){
+         console.log({"data":"","msg":"","err":err.message})
+         res.status(500).json({"data":"","message":"","err":err.message}); 
+     }
+ }
 const readSpecificExpense=async (req,res)=>{
     try{
     let expense = await expenseModel.find({"item":req.query.item});
@@ -52,7 +51,7 @@ const createExpense=async(req,res)=>{
     }
 }
 const createIncome=async(req,res)=>{
-    try{
+  try{
         let data = req.body;
         console.log(data);
         let income = new incomeModel(data);
@@ -64,21 +63,23 @@ const createIncome=async(req,res)=>{
         res.status(500).json({"data":"","message":"","err": err.message });
     }
 }
-const updateExpense=async(req,res)=>{
-    try{
-        let data = req.body;
-        let expense=await expenseModel.updateOne({"item":data.item},{$set: data});
-        (expense.modifiedCount>0)? 
-        res.status(200).json({"data":"","message":"update success","err":""}):
-        res.status(404).json({"data":"","message":"No expense Found","err": "" });
-    }catch(err){
-        res.status(500).json({"data":"","message":"","err":err.message}); 
-    } 
-}
 const deleteExpense=async(req,res)=>{
     try{
-        let expense = await expenseModel.deleteOne({"item":req.params.item});
+        let expense = await expenseModel.deleteMany({});
         (expense.deletedCount > 0) 
+        ?
+            res.status(200).json({"data":"","msg":"delete success","err":""})
+        :
+            res.status(404).json({"data":"","msg":"No expense found","err":""});
+        }
+    catch(err){
+        res.status(500).json({"data":"","msg":"","err":err.message});
+    }
+}
+const deleteIncome=async(req,res)=>{
+    try{
+        let income = await incomeModel.deleteMany({});
+        (income.deletedCount > 0) 
         ?
             res.status(200).json({"data":"","msg":"delete success","err":""})
         :
@@ -94,6 +95,6 @@ module.exports={
     readSpecificExpense,
     createExpense,
     createIncome,
-    updateExpense,
     deleteExpense,
+    deleteIncome
 }
