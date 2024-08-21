b4e6dc3e11b9c99c85ab79ad19ae8ff966a057bc
DelinVijay <152050629+DelinVijay@users.noreply.github.com>
2024-08-21 09:43:06 +0530

    Update express.js

--- src/express.js
+++ src/express.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const app = express();
 const cors = require('cors'); 
-
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 const conn = require('../src/services/db');
 conn.dbConnection();
-
 const useRoute = require('./routes/expenseRoutes');
 app.use('/api/v1/users', useRoute);
-
 app.use('/*', (req, res) => {
   res.send("No Router Match found");
 });
-
 app.listen(4000, () => console.log("Connected to port 4000"));
